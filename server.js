8541b597e828bacf1ae86242ea69bf3f48f356aa
Dennis Chi <dcyttw@gmail.com>
2018-04-14 09:54:04 -0500

    DB Connection Update

--- server.js
+++ server.js
@@ -27,7 +27,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/testScrape");
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/testScrape";
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
 
 // Routes
 // A GET route for scraping the echoJS webiste: no duplicates.
